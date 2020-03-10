41ef97e1b719377b70637d23200f23bfc961e734
yangzhen <p7_1994@163.com>
2020-03-10 21:08:20 +0800

    添加注释

--- src/main/java/com/p7/framework/http/push/constant/PushStatusEnum.java
+++ src/main/java/com/p7/framework/http/push/constant/PushStatusEnum.java
@@ -7,16 +7,34 @@ package com.p7.framework.http.push.constant;
  **/
 public enum PushStatusEnum {
 
+    /**
+     * 推送成功
+     */
     PUSH_SUCCESS(0),
 
+    /**
+     * 响应数据解析失败
+     */
     RESPONSE_DATA_ERROR(1),
 
+    /**
+     * 远程调用失败
+     */
     REMOTE_CALL_FAILED(2),
 
+    /**
+     * 本地错误
+     */
     LOCAL_ERROR(3),
 
+    /**
+     * 重试限制
+     */
     RETRY_LIMITED(4),
 
+    /**
+     * 等待推送
+     */
     PUSH_WAIT(5);
 
     private int pushState;
